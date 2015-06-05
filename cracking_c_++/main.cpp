c505d08fbe8cbd42a02697bcff00276776c237e7
minhnh13 <minhnh13@fsoft.com.vn>
2015-06-05 14:52:52 +0100

    runable

--- cracking_c_++/main.cpp
+++ cracking_c_++/main.cpp
@@ -5,9 +5,28 @@
  *      Author: MinhNH13
  */
 #include <iostream>
+#include <string>
+
+bool is_unique(std::string input)
+{
+	bool char_map[256] = { false };
+	for (size_t i = 0; i < input.length(); ++i)
+	{
+		if (!char_map[(int)input[i]])
+			char_map[(int)input[i]] = true;
+		else
+			return false;
+	}
+	return true;
+}
 
 int main(int argc, char **argv)
 {
+	using namespace std;
+	string input;
+	getline(cin, input);
+	cout << is_unique(input) << endl;
+
 	return 0;
 }
 
