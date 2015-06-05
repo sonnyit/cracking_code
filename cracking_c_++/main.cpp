e2f4a343a4f7d08643620a0ad594f45daea0cabf
minhnh13 <minhnh13@fsoft.com.vn>
2015-06-05 15:04:02 +0100

    runable

--- cracking_c_++/main.cpp
+++ cracking_c_++/main.cpp
@@ -6,8 +6,29 @@
  */
 #include <iostream>
 
+void reverse_string(char *str)
+{
+	char *end = str;
+	while (*end != '\0')
+		end++;
+	end--;
+	while(str < end)
+	{
+		char temp = *str;
+		*str = *end;
+		*end = temp;
+		end--;
+		str++;
+	}
+}
+
 int main(int argc, char **argv)
 {
+	using namespace std;
+	char input[256];
+	cin.getline(input, 256);
+	reverse_string(input);
+	cout << input << endl;
 	return 0;
 }
 
