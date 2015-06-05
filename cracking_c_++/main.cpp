21923eb1b0086e4cda25f0387eda91cd1b69714a
minh nguyen <nhmjnh@gmail.com>
2015-06-05 23:13:51 +0100

    runable anagrams check

--- cracking_c_++/main.cpp
+++ cracking_c_++/main.cpp
@@ -5,9 +5,30 @@
  *      Author: MinhNH13
  */
 #include <iostream>
+#include <string>
+
+bool is_anagrams(std::string str1, std::string str2)
+{
+    std::sort(str1.begin(), str1.end());
+    std::sort(str2.begin(), str2.end());
+    if (str1.compare(str2) == 0)
+    {
+        return true;
+    }
+    return false;
+}
 
 int main(int argc, char **argv)
 {
+    using namespace std;
+    string str1;
+    string str2;
+    cout << "input str1: " << endl;
+    getline(cin, str1);
+    cout << "input str2: " << endl;
+    getline(cin, str2);
+    
+    cout << is_anagrams(str1, str2) << endl;
 	return 0;
 }
 
