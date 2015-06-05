96e03e5273eabafdd57a67e1a4757717096b1425
minhnh13 <minhnh13@fsoft.com.vn>
2015-06-05 17:12:39 +0100

    runable

--- cracking_c_++/main.cpp
+++ cracking_c_++/main.cpp
@@ -7,10 +7,32 @@
 #include <iostream>
 #include <string>
 
+void remove_duplicate(std::string &str) {
+	unsigned check = 1;
+	unsigned tail = 1;
+
+	for (; check < str.length(); ++check) {
+		unsigned j;
+		for (j = 0; j < tail; ++j) {
+			if (str[check] == str[j])
+				break;
+		}
+		if (j == tail) {
+			str[tail] = str[check];
+			tail++;
+		}
+	}
+	str[tail] = 0;
+}
+
 int main(int argc, char **argv)
 {
-	std::string input;
-	getline(input, 256);
+	using namespace std;
+	string input;
+	getline(cin, input);
+	remove_duplicate(input);
+	cout << input.c_str() << endl;
+
 	return 0;
 }
 
